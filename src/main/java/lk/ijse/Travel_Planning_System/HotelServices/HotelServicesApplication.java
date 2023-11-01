af4bf2ad80250bd82ea59f48365e4d94fe02491b
KasunUmesh <kasunumesh38@gmail.com>
2023-11-02 05:00:19 +0530

    init Project

---
+++ src/main/java/lk/ijse/Travel_Planning_System/HotelServices/HotelServicesApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Travel_Planning_System.HotelServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServicesApplication.class, args);
+	}
+
+}
