31d168db4a1bbbb51dfdef0e6ee84eedb6eecb7a
KasunUmesh <kasunumesh38@gmail.com>
2023-11-05 04:11:51 +0530

    created application.properties and packages

--- src/main/java/lk/ijse/Travel_Planning_System/HotelServices/HotelServicesApplication.java
+++ src/main/java/lk/ijse/Travel_Planning_System/HotelServices/HotelServicesApplication.java
@@ -2,8 +2,9 @@ package lk.ijse.Travel_Planning_System.HotelServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HotelServicesApplication {
 
 	public static void main(String[] args) {
